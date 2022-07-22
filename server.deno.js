80b1fc51fdc853e8b536348fb9d33e05c2c749d9
Jack Merrill <me@jackmerrill.com>
2022-07-22 00:41:16 -0500

    feat: add sentry

--- server.deno.js
+++ server.deno.js
@@ -1,1 +1,8 @@
+import * as Sentry from "https://deno.land/x/sentry_deno/main.ts";
 import "./dist/server/entry.mjs";
+
+Sentry.init({
+  dsn: "https://dee54b8cee614b7eb6ae97dafff697b0@gt.astralapp.io/2",
+});
+
+Sentry.captureMessage("Hello World");
