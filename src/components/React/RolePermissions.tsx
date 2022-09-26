d7f0792dfd5dc5fd290da43c969cadd4b964a682
Jack <me@jackmerrill.com>
2022-09-25 23:23:22 -0400

    feat: add saving permissions

--- src/components/React/RolePermissions.tsx
+++ src/components/React/RolePermissions.tsx
@@ -28,14 +28,14 @@ export default function RolePermissions({
 
   return (
     <>
-      <div className="grid grid-cols-3 col-span-full">
-        <div className="flex rounded-l-md shadow-sm col-span-2">
+      <div className="grid grid-cols-5 col-span-full">
+        <div className="flex rounded-l-md shadow-sm col-span-4">
           <span className="inline-flex items-center px-3 text-white rounded-l-md bg-black-400 sm:text-sm">
             @
           </span>
           <select
-            id="channel"
-            name="settings.modules.moderation.options.logging.channel"
+            id="role"
+            name="role"
             className="flex-1 block w-full min-w-0 px-1 py-2 border-none rounded-none bg-black-400 sm:text-sm"
             onChange={(e) => {
               setSelectedRole(e.target.value);
@@ -79,28 +79,27 @@ export default function RolePermissions({
 
       {perms.roles &&
         Object.entries(perms.roles).map(([role, perm], i) => (
-          <div className="grid grid-cols-2 col-span-full">
-            <div className="flex rounded-l-md shadow-sm col-span-1">
-              <span className="inline-flex items-center px-3 text-white rounded-l-md bg-black-400 sm:text-sm">
+          <div className="grid grid-cols-5 col-span-full">
+            <div className="flex shadow-sm md:col-span-1 col-span-full rounded-md">
+              <span className="inline-flex items-center px-3 text-white rounded-l-md rounded-bl-none md:rounded-bl-md bg-black-400 sm:text-sm">
                 @
               </span>
-              <input
-                type="text"
-                readOnly
-                value={roles.find((r) => r.id === role)?.name || role}
-                className="flex-1 block w-full min-w-0 px-1 py-2 border-none rounded-none bg-black-400 sm:text-sm"
-              />
+              <p className="flex-1 flex items-center w-full min-w-0 px-2 py-2 border-none md:rounded-none rounded-r-md rounded-br-none bg-black-400 sm:text-sm truncate">
+                {roles.find((r) => r.id === role)?.name || role}
+              </p>
             </div>
 
-            <div className="col-span-1">
+            <div className="md:col-span-3 col-span-full">
               <ReactChip
                 defaultChips={perm}
                 id={`role-permissions-${i}`}
                 name={`permissions.roles.${role}`}
                 labelClass="bg-black-300 flex flex-wrap rounded-r-md"
-                inputClass="bg-black-300 text-white px-3 py-2 border-none rounded-r-md w-full"
+                inputClass="bg-black-300 text-white px-3 py-2 border-none w-full"
                 chipClass="bg-black-400 text-white hover:bg-black-500 transition duration-200 p-2 rounded-md m-2"
                 focusClass="bg-black-500"
+                // regex should match the domain syntax ((+/-)astral.{category or *}.{command or *})
+                regex={/^(\+|-)?astral\.(?:\*|[\w-]+)\.(?:\*|[\w-]+)$/}
                 onChange={(chips) => {
                   setPerms((p) => ({
                     ...p,
@@ -112,6 +111,29 @@ export default function RolePermissions({
                 }}
               />
             </div>
+
+            <button
+              type="button"
+              className="bg-astral md:rounded-r-md rounded-b-md md:rounded-bl-none text-white font-bold px-4 py-2 md:col-span-1 col-span-full"
+              onClick={() => {
+                setPerms((p) => {
+                  const newPerms = { ...p };
+                  delete newPerms.roles[role];
+                  return newPerms;
+                });
+
+                setAvailableRoles((r) => {
+                  // add the role to the available roles
+                  const newRoles = [...r, roles.find((r) => r.id === role)];
+                  // if the selected role is not in the new roles, set the selected role to the first role
+                  if (!newRoles.map((role) => role.id).includes(selectedRole))
+                    setSelectedRole(newRoles[0].id);
+                  return newRoles;
+                });
+              }}
+            >
+              Remove
+            </button>
           </div>
         ))}
     </>
